23b7fcb3f495d6953ebc9b79835f188b8c9513ff
snarvekark <56081922+snarvekark@users.noreply.github.com>
2019-10-19 01:19:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
- consumer_key:         'VQjW3MvSjTnjkb1iYmltnCsCa',
-  consumer_secret:      'mOlmQUXclpOiZGNRabXFEMGmG8Tp9dglQhKF0qS5qC1wwNQPig',
-  access_token:         '1175972561542234112-l7S2AABDmsMTWAtRE0Z9cvO2nDdGJc',
-  access_token_secret:  'wlNfxpoiGNB9WuPEIQ4zVeADHv40LMPkL8PMWW6PXZHSS'
+ consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
